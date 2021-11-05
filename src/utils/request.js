db63191753e14f5ab4cc8610ebec030fae13aa14
kaite_han@163.com <kaite_han@163.com>
2021-11-05 18:23:21 +0800

    完成头部样式

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,12 +1,23 @@
 import axios from 'axios'
 import { Message } from 'element-ui'
+import store from '@/store'
 const service = axios.create({
   // 如果执行 npm run dev  值为 /api 正确  /api 这个代理只是给开发环境配置的代理
   // 如果执行 npm run build 值为 /prod-api  没关系  运维应该在上线的时候 给你配置上 /prod-api的代理
   baseURL: process.env.VUE_APP_BASE_API, // 设置axios请求的基础的基础地址
   timeout: 5000 // 定义5秒超时
 })
-service.interceptors.request.use()
+// 请求拦截器
+service.interceptors.request.use(config => {
+  // config是请求配置信息
+  if (store.getters.token) {
+    config.headers['Authorization'] = `Bearer ${store.getters.token}`
+  }
+  return config // 必须返回
+}, error => {
+  return Promise.reject(error)
+})
+// 响应拦截器
 service.interceptors.response.use(response => {
   // axios默认加了一层data
   const { success, message, data } = response.data
