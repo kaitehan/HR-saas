42b086803ec81545527c766a46a623e6c398ff09
kaite_han@163.com <kaite_han@163.com>
2021-11-05 11:01:04 +0800

    封装单独的登录接口

--- src/api/user.js
+++ src/api/user.js
@@ -1,7 +1,12 @@
-// import request from '@/utils/request'
+import request from '@/utils/request'
 
 export function login(data) {
+  return request({
+    method: 'POST',
+    url: '/sys/login',
+    data
 
+  })
 }
 
 export function getInfo(token) {
