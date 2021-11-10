f757a2669dadcdd748b07b4d479187322645f499
kaite_han@163.com <kaite_han@163.com>
2021-11-10 18:15:29 +0800

    完成了excel导出功能

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -10,7 +10,7 @@
         <!-- 右侧显示按钮 excel导入  excel导出  新增员工 -->
         <template v-slot:after>
           <el-button type="success" size="small" @click="$router.push('/import')">excel导入</el-button>
-          <el-button type="danger" size="small">excel导出</el-button>
+          <el-button type="danger" size="small" @click="exportData">excel导出</el-button>
           <el-button type="primary" size="small" @click="showDialog= true">新增员工</el-button>
         </template>
       </page-tools>
@@ -72,6 +72,7 @@
 import { getEmployeeList, delEmployee } from '@/api/employees'
 import EmployeeEnum from '@/api/constant/employees' // 引入员工 的枚举对象
 import AddEmployee from './components/add-employee.vue'
+import { formatDate } from '@/filters'
 export default {
   components: {
     AddEmployee
@@ -132,6 +133,68 @@ export default {
           message: '已取消删除'
         })
       })
+    },
+    exportData() {
+      const headers = {
+        '手机号': 'mobile',
+        '姓名': 'username',
+        '入职日期': 'timeOfEntry',
+        '聘用形式': 'formOfEmployment',
+        '转正日期': 'correctionTime',
+        '工号': 'workNumber',
+        '部门': 'departmentName'
+      }
+      // 导出EXCEL
+      import('@/vendor/Export2Excel').then(async excel => {
+        // excel 是引入文件的导出对象
+        // 导出  header从哪来？
+        // data 从哪来
+        // Object.keys(headers) 表头
+        const { rows } = await getEmployeeList({ page: 1, size: this.page.total })
+        const data = this.formatJson(rows, headers)
+        const multiHeader = [['姓名', '主要信息', '', '', '', '', '部门']]
+        const merges = ['A1:A2', 'B1:F1', 'G1:G2']
+        excel.export_json_to_excel({
+          header: Object.keys(headers),
+          data,
+          filename: '员工列表',
+          autoWidth: true,
+          bookType: 'xlsx',
+          multiHeader, // 复杂表头
+          merges// 合并选项
+        })
+        // excel.export_json_to_excel({
+        //   header: ['姓名', '工资'],
+        //   data: [['李四', 3000], ['张三', 2000]],
+        //   filename: '员工信息表',
+        //   autoWidth: true,
+        //   bookType: 'xlsx'
+        // })
+        // [{username:'张三',mobile:13455556666}] => [[]] 需要
+        // 要转化 数据结构 还要和表头的顺序对应上
+        // 要求转出的标题是中文
+      })
+    },
+    // 将表头数据和数据进行对应
+    // [{}] => [[]]
+    formatJson(data, headers) {
+      return data.map(item => {
+        // item 是一个对象 {mobile:13135353333,username:'张三'}
+        // 要转成 ["手机号","姓名",...]
+        return Object.keys(headers).map(obj => {
+          if (headers[obj] === 'timeOfEntry' || headers[obj] === 'correctionTime') {
+            return formatDate(item[headers[obj]])
+          } else if (headers[obj] === 'formOfEmployment') {
+            // return item[headers[obj]] === 1 ? '正式' : '非正式'
+            const res = EmployeeEnum.hireType.find(x => x.id === item[headers[obj]])
+            return res ? res.value : '未知'
+          }
+          return item[headers[obj]]
+        })
+        // ['13135353333','张三']
+      })
+      // 精简代码
+      // return data.map(item => Object.keys(headers).map(obj => item[headers[obj]]))
     }
   }
 
